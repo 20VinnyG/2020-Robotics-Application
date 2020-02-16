d6bb0fe2513e9385a707f6ec582531496df7e96c
nvoidknight <necroviralknightprime@gmail.com>
2020-02-15 22:55:50 -0500

    replace all leading spaces with tabs

--- lib/match.dart
+++ lib/match.dart
@@ -2,32 +2,32 @@ import 'package:flutter/material.dart';
 import 'shot.dart';
 
 class MatchData {
-  //Pregame
-  String initials = '';
-  int position;
-  int matchNumber;
-  int teamNumber;
-  double initiationlinepos = 0;
-  int preloadedfuelcells;
-  int id;
-  //Auton
-  List<Offset> autopathpoints = [];
-  List<int> autopathx = [];
-  List<int> autopathy = [];
-  List<Shot> autoshots = [];
-  //Telop
-  List<Shot> teleopshots = [];
-  //Endgame
-  double climbtime=0;
-  int park;
-  bool levelability = false;
-  bool assist = false;
-  bool typeassist = false;
-  //Postgame
-  double generalSuccess =  0;
-  double defensiveSuccess =0;
-  double accuracy = 0;
-  bool floorpickup = false;
-  bool fouls = false;
-  bool problems = false;
+	//Pregame
+	String initials = '';
+	int position;
+	int matchNumber;
+	int teamNumber;
+	double initiationlinepos = 0;
+	int preloadedfuelcells;
+	int id;
+	//Auton
+	List<Offset> autopathpoints = [];
+	List<int> autopathx = [];
+	List<int> autopathy = [];
+	List<Shot> autoshots = [];
+	//Telop
+	List<Shot> teleopshots = [];
+	//Endgame
+	double climbtime=0;
+	int park;
+	bool levelability = false;
+	bool assist = false;
+	bool typeassist = false;
+	//Postgame
+	double generalSuccess =	0;
+	double defensiveSuccess =0;
+	double accuracy = 0;
+	bool floorpickup = false;
+	bool fouls = false;
+	bool problems = false;
 }
--- lib/teleop.dart
+++ lib/teleop.dart
@@ -10,140 +10,140 @@ Canvas canvas;
 Size size = new Size(screenx, screeny);
 
 class Teleop extends StatefulWidget {
-  // final List<Shot> teleopshotsList;
-  final MatchData matchData;
+	// final List<Shot> teleopshotsList;
+	final MatchData matchData;
 
-  @override
-  _TeleopState createState() => _TeleopState();
+	@override
+	_TeleopState createState() => _TeleopState();
 
-  Teleop({
-    this.matchData
-  });
+	Teleop({
+		this.matchData
+	});
 }
 
 bool val = true;
 
 class _TeleopState extends State<Teleop> {
 
-  void onTapDown(BuildContext context, TapDownDetails details) {
-    print('${details.globalPosition}');
-    final RenderBox box = context.findRenderObject();
-    final Offset localOffset = box.globalToLocal(details.globalPosition);
-    setState(() {
-      screenx = localOffset.dx;
-      screeny = localOffset.dy;
-    });
-  }
+	void onTapDown(BuildContext context, TapDownDetails details) {
+		print('${details.globalPosition}');
+		final RenderBox box = context.findRenderObject();
+		final Offset localOffset = box.globalToLocal(details.globalPosition);
+		setState(() {
+			screenx = localOffset.dx;
+			screeny = localOffset.dy;
+		});
+	}
 
-  onSwitchValueChanged(bool newVal) {
-    setState(() {
-      val = newVal;
-    });
-  }
+	onSwitchValueChanged(bool newVal) {
+		setState(() {
+			val = newVal;
+		});
+	}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: new GestureDetector(
-          child: new Stack(
-        children: <Widget>[
-          new Container(
-            decoration: new BoxDecoration(
-                image: new DecorationImage(
-                    image: new AssetImage('assets/images/field.png'),
-                    fit: BoxFit.cover)),
-          )
-        ],
-      ),
-      // floatingActionButton: SpeedDial(
-      //     animatedIcon: AnimatedIcons.menu_close,
-      //     children: [
-      //       SpeedDialChild(
-      //           backgroundColor: Colors.red,
-      //           child: Icon(Icons.clear),
-      //           label: "Clear Path",
-      //           onTap: () => points.clear()),
-      //       SpeedDialChild(
-      //           backgroundColor: Colors.green,
-      //           child: Icon(Icons.check),
-      //           label: "Completed Path",
-      //           onTap: () {
-      //             condensePoints();
-      //             Navigator.pop(context);
-      //           })
-      //     ],
-      //   ));
-      onTapDown: (TapDownDetails details) => onTapDown(context, details),
-      onTap: () {
-        Shot newShot = new Shot();
-        newShot.posx = screenx;
-        newShot.posy = screeny;
-        print(newShot.posx);
-        print(newShot.posy);
-        return showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                  title: Text("Enter Number of Balls Made"),
-                  content: ListView(
-                    children: <Widget>[
-                      DropdownButton(
-                        items: [
-                          DropdownMenuItem(
-                              value: int.parse("0"), child: Text("0")),
-                          DropdownMenuItem(
-                              value: int.parse("1"), child: Text("1")),
-                          DropdownMenuItem(
-                              value: int.parse("2"), child: Text("2")),
-                          DropdownMenuItem(
-                              value: int.parse("3"), child: Text("3")),
-                          DropdownMenuItem(
-                              value: int.parse("4"), child: Text("4")),
-                          DropdownMenuItem(
-                              value: int.parse("5"), child: Text("5")),
-                        ],
-                        onChanged: (value) {
-                          setState(() {
-                            newShot.shotsMade = value;
-                          });
-                        },
-                        hint: Text("Balls Made"),
-                        value: newShot.shotsMade,
-                      ),
-                      Switch(
-                          value: newShot.shotType,
-                          onChanged: (bool s) {
-                            setState(() {
-                              newShot.shotType = s;
-                            });
-                          },
-                        ),
-                      RaisedButton(
-                        child: Text("Done"),
-                        onPressed: () {
-                          print(newShot.shotsMade);
-                          print(newShot.shotType);
-                          print(newShot.toString());
-                          widget.matchData.teleopshots.add(newShot);
-                          Navigator.pop(context);
-                        },
-                      )
-                    ],
-                  ));
-            });
-      },
-    ));
-  }
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+				body: new GestureDetector(
+					child: new Stack(
+				children: <Widget>[
+					new Container(
+						decoration: new BoxDecoration(
+								image: new DecorationImage(
+										image: new AssetImage('assets/images/field.png'),
+										fit: BoxFit.cover)),
+					)
+				],
+			),
+			// floatingActionButton: SpeedDial(
+			//		 animatedIcon: AnimatedIcons.menu_close,
+			//		 children: [
+			//			 SpeedDialChild(
+			//					 backgroundColor: Colors.red,
+			//					 child: Icon(Icons.clear),
+			//					 label: "Clear Path",
+			//					 onTap: () => points.clear()),
+			//			 SpeedDialChild(
+			//					 backgroundColor: Colors.green,
+			//					 child: Icon(Icons.check),
+			//					 label: "Completed Path",
+			//					 onTap: () {
+			//						 condensePoints();
+			//						 Navigator.pop(context);
+			//					 })
+			//		 ],
+			//	 ));
+			onTapDown: (TapDownDetails details) => onTapDown(context, details),
+			onTap: () {
+				Shot newShot = new Shot();
+				newShot.posx = screenx;
+				newShot.posy = screeny;
+				print(newShot.posx);
+				print(newShot.posy);
+				return showDialog(
+						context: context,
+						builder: (context) {
+							return AlertDialog(
+									title: Text("Enter Number of Balls Made"),
+									content: ListView(
+										children: <Widget>[
+											DropdownButton(
+												items: [
+													DropdownMenuItem(
+															value: int.parse("0"), child: Text("0")),
+													DropdownMenuItem(
+															value: int.parse("1"), child: Text("1")),
+													DropdownMenuItem(
+															value: int.parse("2"), child: Text("2")),
+													DropdownMenuItem(
+															value: int.parse("3"), child: Text("3")),
+													DropdownMenuItem(
+															value: int.parse("4"), child: Text("4")),
+													DropdownMenuItem(
+															value: int.parse("5"), child: Text("5")),
+												],
+												onChanged: (value) {
+													setState(() {
+														newShot.shotsMade = value;
+													});
+												},
+												hint: Text("Balls Made"),
+												value: newShot.shotsMade,
+											),
+											Switch(
+													value: newShot.shotType,
+													onChanged: (bool s) {
+														setState(() {
+															newShot.shotType = s;
+														});
+													},
+												),
+											RaisedButton(
+												child: Text("Done"),
+												onPressed: () {
+													print(newShot.shotsMade);
+													print(newShot.shotType);
+													print(newShot.toString());
+													widget.matchData.teleopshots.add(newShot);
+													Navigator.pop(context);
+												},
+											)
+										],
+									));
+						});
+			},
+		));
+	}
 }
 
 class MyPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    canvas.drawCircle(Offset(screenx, screeny), 25.0, Paint());
-  }
+	@override
+	void paint(Canvas canvas, Size size) {
+		canvas.drawCircle(Offset(screenx, screeny), 25.0, Paint());
+	}
 
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return null;
-  }
+	@override
+	bool shouldRepaint(CustomPainter oldDelegate) {
+		return null;
+	}
 }
